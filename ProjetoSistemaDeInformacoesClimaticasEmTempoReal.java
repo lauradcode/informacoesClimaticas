5bf82f341b9870bc80b825cb6e9b1f2a34e9150e
Laura Duarte <laura.bduartesouza@gmail.com>
2026-01-04 08:59:12 -0300

    add lib

--- ProjetoSistemaDeInformacoesClimaticasEmTempoReal.java
+++ ProjetoSistemaDeInformacoesClimaticasEmTempoReal.java
@@ -1,1 +1,17 @@
-public class ProjetoSistemaDeInformacoesClimaticasEmTempoReal {}
+import org.json.JSONObject;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ProjetoSistemaDeInformacoesClimaticasEmTempoReal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // definindo o scanner para ler do teclado o local (System.in)
+
+    }    
+}
