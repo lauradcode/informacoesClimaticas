2a0dc8e4622a38a2fe706a2e5606a7d4a914e2e7
Laura Duarte <laura.bduartesouza@gmail.com>
2026-01-04 12:10:40 -0300

    SEQUêNCIA
    COLETANDO os dados e imprimindo de forma organizada. As String são de acordo com a documentação do Weather API

--- ProjetoSistemaDeInformacoesClimaticasEmTempoReal.java
+++ ProjetoSistemaDeInformacoesClimaticasEmTempoReal.java
@@ -29,9 +29,49 @@ public class ProjetoSistemaDeInformacoesClimaticasEmTempoReal {
     }
     
     public static String getDadosClimaticos (String cidade) throws Exception { //desenvolvendo o método mencionado anteriomente
+        String apiKey = Files.readString( Paths.get("api-key.txt")).trim(); /* colocando a api coletada. trim -> remove espaço errado */
+        String formataNomeCidade = URLEncoder.encode(cidade, StandardCharsets.UTF_8);
+        String apiUrl = "http://weatherapi.com/v1/current.json?key=" + apiKey + "&q=" + formataNomeCidade; //escrevendo a url, isso é uma string
+        HttpRequest request = HttpRequest.newBuilder() //classe java que vai sair do sistema e fazer a solicitação 
+            .uri(URI.create(apiUrl)) //define o URI da solicitação http
+            .build(); //finaliza a construção da solicitação HTTP
+        
+        //Criando objeto para enviar a solicitação HTTP e receber a resposta HTTP para acessar o site weather api
+        HttpClient client = HttpClient.newHttpClient(); //objeto com poder de enviar solicitação e receber resposta
+        //Criando objeto para enviar a requisição HTTP e receber a resposta HTTP, para se comunicar com o site 
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+        return response.body(); //retorna os dados obtidos 
     }
 
+        /*fazendo o método da impressão para não ficar confuso para o usuário. TODAS as Strings usadas são de acordo com a DOCUMENTAÇÃO DO WEATHER API*/
+    public static void imprimirDadosClimaticos(String dados){
+        JSONObject dadosJson = new JSONObject(dados); //aqui esta od dados da localização
+        JSONObject informacoesMeteorologicas = dadosJson.getJSONObject("current"); //significa que quero os dados em tempo real da weather, por isso 'current'
 
+            //extrai os dados da localização
+        String cidade = dadosJson.getJSONObject("location").getString ("name");
+        String pais = dadosJson.getJSONObject("location").getString ("country");
 
+            //extrai dados adicionais 
+        String condicaoTempo = informacoesMeteorologicas.getJSONObject("condition").getString("text"); //condição e descrição
+        int umidade = informacoesMeteorologicas.getInt("humidity");
+        float velocidadeVento = informacoesMeteorologicas.getFloat("wind_kph");
+        float pressaoAtmosferica = informacoesMeteorologicas.getFloat("pressure_mb");
+        float sensacaoTermica = informacoesMeteorologicas.getFloat("feelslike_c");
+        float temperaturaAtual = informacoesMeteorologicas.getFloat("temp_c");
+
+            // Extrai a data e hora da string retornada pela API
+        String dataHoraString = informacoesMeteorologicas.getString("last_updated");
+
+            // Imprime as informações atuais
+        System.out.println("Informações Meteorológicas para " + cidade + ", " + pais);
+        System.out.println("Data e Hora: " + dataHoraString);
+        System.out.println("Temperatura Atual: " + temperaturaAtual + "°C");
+        System.out.println("Sensação Térmica: " + sensacaoTermica + "°C");
+        System.out.println("Condição do Tempo: " + condicaoTempo);
+        System.out.println("Umidade: " + umidade + "%");
+        System.out.println("Velocidade do Vento: " + velocidadeVento + " km/h");
+        System.out.println("Pressão Atmosférica: " + pressaoAtmosferica + " mb");
+        }
 }
